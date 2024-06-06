bc615cf5d1f1b490610da4eba6acfc97a1474b67
yamada <dummy@example.com>
2024-06-06 14:47:39 +0900

    2024年6月6日　ログイン画面・メニュー画面の画面レイアウトは、ほぼ完成。ログイン画面から新規ユーザー登録画面に遷移はできない(セッションエラーが発生中)。メニュー画面で新規ユーザー登録画面を作成してから、ログイン画面にできたものを持っていく。(追加機能を持たせたもの。)

---
+++ src/cashbook/action/common/YuzaNewAction.java
@@ -0,0 +1,41 @@
+package cashbook.action.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import cashbook.dto.common.LoginDto;
+import cashbook.util.Const;
+
+/**
+ * メニュー画面アクションクラス
+ * @author soppra
+ */
+public class YuzaNewAction extends BaseAction {
+
+	/**
+	 * <p><b>
+	 * メニュー画面
+	 * <br>初期表示処理
+	 * </b></p>
+	 *
+	 *MenulnitActionの真似をしている
+	 *
+	 * @param map      アクションマッピング
+	 * @param form     フォーム
+	 * @param request  リクエスト
+	 * @param response レスポンス
+	 * @param loginDto ログイン情報DTO
+	 * @return アクションフォワード
+	 * @throws Exception すべての例外
+	 */
+	protected ActionForward doProcess(ActionMapping map, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response, LoginDto loginDto) throws Exception {
+
+		// 処理成功時の遷移先を指定する。
+		return map.findForward(Const.ACTION_FOWARD_SUCCESS);
+	}
+}
